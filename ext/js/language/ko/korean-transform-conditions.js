03e25d5593495cb0dc7cf9892f2c83adf77445fe
sottaku <hello@sottaku.app>
2026-08-03 05:24:57 +0900

    refactor: split Korean transform conditions

---
+++ ext/js/language/ko/korean-transform-conditions.js
@@ -0,0 +1,97 @@
+/*
+ * Copyright (C) 2024-2026  Yomitan Authors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+export const conditions = {
+    v: {
+        name: 'Verb',
+        isDictionaryForm: true,
+        i18n: [
+            {
+                language: 'ko',
+                name: '동사 / 보조 동사',
+            },
+        ],
+    },
+    adj: {
+        name: 'Adjective',
+        isDictionaryForm: true,
+        i18n: [
+            {
+                language: 'ko',
+                name: '형용사 / 보조 형용사',
+            },
+        ],
+    },
+    ida: {
+        name: 'Postpositional particle ida',
+        isDictionaryForm: true,
+        i18n: [
+            {
+                language: 'ko',
+                name: '조사 이다',
+            },
+        ],
+    },
+    p: {
+        name: 'Intermediate past tense ending',
+        isDictionaryForm: false,
+    },
+    f: {
+        name: 'Intermediate future tense ending',
+        isDictionaryForm: false,
+    },
+    eusi: {
+        name: 'Intermediate formal ending',
+        isDictionaryForm: false,
+    },
+    euob: {
+        name: 'Intermediate formal ending',
+        isDictionaryForm: false,
+    },
+    euo: {
+        name: 'Intermediate formal ending',
+        isDictionaryForm: false,
+    },
+    sao: {
+        name: 'Intermediate formal ending',
+        isDictionaryForm: false,
+    },
+    saob: {
+        name: 'Intermediate formal ending',
+        isDictionaryForm: false,
+    },
+    sab: {
+        name: 'Intermediate formal ending',
+        isDictionaryForm: false,
+    },
+    jaob: {
+        name: 'Intermediate formal ending',
+        isDictionaryForm: false,
+    },
+    jao: {
+        name: 'Intermediate formal ending',
+        isDictionaryForm: false,
+    },
+    jab: {
+        name: 'Intermediate formal ending',
+        isDictionaryForm: false,
+    },
+    do: {
+        name: 'Intermediate ending',
+        isDictionaryForm: false,
+    },
+};
